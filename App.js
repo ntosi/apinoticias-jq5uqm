f58b11f87b4e5d190c6c46ad35f1f221e8541a65
ntosi <nicolas.tosi@gmail.com>
2019-08-26 19:36:00 -0300

    1 file modified

--- App.js
+++ App.js
@@ -1,42 +1,36 @@
-import React,{Fragment, Component} from 'react';
+import React,{Fragment, Component, useState} from 'react';
 import Header from './componentes/Header';
 import Formulario from './componentes/Formulario';
 import Noticias from './componentes/Noticias';
 
 
 
-export default class App extends Component {
-  state={
-    noticias:[]
-  }
-// component did mount se fija que se cargue el componente
-  componentDidMount(){
-    this.consultarNoticias()
 
-  }
+
+function App() {
+  const [noticias, setNoticias] = useState([])
   
-  consultarNoticias = async (categoria="general") => {
+  const consultarNoticias = async (categoria="general") => {
     const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=f8fcf699dc81467d803c1f8a12debad9`
     const respuesta = await fetch (url)
-    const noticias = await respuesta.json()
-    // console.log(noticias.articles)
-    this.setState({
-      noticias: noticias.articles
-    })
+    const noti = await respuesta.json()
+    console.log(noti.articles)
+    setNoticias(noti.articles)
+    
   }
   
-  render() {
+
     return (
       <div>
           <Fragment>
             <Header 
                     titulo='Noticias'
             />
-            <Formulario traerNoticias={this.consultarNoticias}/>
-            <Noticias noticias={this.state.noticias}/>
+            <Formulario traerNoticias={consultarNoticias}/>
+            <Noticias noticias={noticias}/>
           </Fragment>    
       </div>
     )
   }
-}
 
+export default App
