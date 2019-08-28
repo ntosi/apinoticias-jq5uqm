1e82825052f7029c52e21e77cd5e1b715dea5568
ntosi <nicolas.tosi@gmail.com>
2019-08-28 20:26:32 -0300

    1 file modified

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import Noticias from './componentes/Noticias';
 
 function App() {
   const [noticias, setNoticias] = useState([])
-  
+  // usar useeffect para meter consultarnoticias
   const consultarNoticias = async (categoria="general") => {
     const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=f8fcf699dc81467d803c1f8a12debad9`
     const respuesta = await fetch (url)
