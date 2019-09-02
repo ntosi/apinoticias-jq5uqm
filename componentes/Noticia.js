356aa2226a4c17e244a4676c5f7289472bb8523d
ntosi <nicolas.tosi@gmail.com>
2019-09-01 23:17:25 -0300

    2 files modified

--- componentes/Noticia.js
+++ componentes/Noticia.js
@@ -14,7 +14,7 @@ export default class Noticia extends Component {
                 <img src={urlToImage} alt="{title}" width="100%" /> <br/>
                     <span className="card-title">{source.name}</span>
                     <div className="card-content">
-                        <h3>{title}</h3>
+                        <h2>{title}</h2>
                         <p>{description}</p>
                     </div>
                     <div className="card-action">
